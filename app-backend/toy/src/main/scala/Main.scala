0cf636b71378763ad7dd5346dded4d5334a3165f
Aaron Su <xunzesu@outlook.com>
2018-03-15 19:17:16 -0400

    Add read local tif endpoint.

--- app-backend/toy/src/main/scala/Main.scala
+++ app-backend/toy/src/main/scala/Main.scala
@@ -7,26 +7,38 @@ import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import scala.io.StdIn
 import com.typesafe.config.{ConfigFactory, Config}
+import geotrellis.raster.io.geotiff.reader.GeoTiffReader
+import geotrellis.raster.io.geotiff._
 
 object WebServer {
-  def main(args: Array[String]) {
 
+  def readRasterData(path: String): SinglebandGeoTiff = {
+    GeoTiffReader.readSingleband(path)
+  }
+
+  def listLocalFile(fileName: String) = complete {
+    val path = s"${System.getProperty("user.dir")}/data/raster/${fileName}"
+    val tiffData = readRasterData(path)
+    HttpEntity(ContentTypes.`text/html(UTF-8)`, s"""<h1>${fileName}</h1>
+      <h2>CRS: ${tiffData.crs}</h2>
+      <h2>Extent: ${tiffData.extent}</h2>""")
+  }
+
+  def main(args: Array[String]) {
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
     val config = ConfigFactory.load()
 
     val routes =
-      get {
-        pathSingleSlash {
-          complete {
-            HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>")
+      pathPrefix("api") {
+        pathPrefix("localfile") {
+          pathPrefix(Segment) {fileName =>
+            get { listLocalFile(fileName) }
           }
         }
       }
 
-    println(config.getString("http.address"))
-    println(config.getInt("http.port"))
-
     Http().bindAndHandle(routes, config.getString("http.address"), config.getInt("http.port"))
   }
+
 }
